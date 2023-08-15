c9070c1ceaf6a1f9d69d9edcff504a19e949811a
FETYagency <fetyagency@gmail.com>
2023-08-15 16:02:25 +0100

    base fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/INTERMIDIATE_comm_RA /",
+  base:"/INTERMIDIATE_comm_RA/",
   plugins: [react()],
 })
